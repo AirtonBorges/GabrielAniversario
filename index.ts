75d7967cf4dee9f2adfc519c47a897e09fb2b7b0
AirtonBorges <42126588+AirtonBorges@users.noreply.github.com>
2023-12-19 10:52:48 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/ZDYwnCFiTA9gMHxn/scene.splinecode');
